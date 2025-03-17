cd23a452ae7e79fdb519daac37e2f5a8517b274a
Aidan <aidanbh16@gmail.com>
2025-03-17 14:38:54 -0400

    Late commit, finished setting up tailwind environment

--- dist/app.js
+++ dist/app.js
@@ -4,8 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const PORT = process.env.PORT || 3000;
 const app = (0, express_1.default)();
+app.use(express_1.default.static(path_1.default.join(__dirname, "../public")));
 const homeRoute_1 = __importDefault(require("./routes/homeRoute"));
 app.use("/", homeRoute_1.default);
 app.listen(PORT, () => {
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import path from "path";
 const PORT = process.env.PORT || 3000;
-
 const app = express();
+
+app.use(express.static(path.join(__dirname, "../public")));
 
 import homeRoute from "./routes/homeRoute";
 
