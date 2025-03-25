32f3c2abeda6f9ed63135f74971df9197be59565
Aidan <aidanbh16@gmail.com>
2025-03-25 15:39:16 -0400

    Added more tailwind and expanded on experience section

---
+++ public/scroll.js
@@ -0,0 +1,20 @@
+document.getElementById("home").onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
+
+document.getElementById("experience").onclick = function () {
+    window.scrollTo({
+        top: 950,
+        behavior: "smooth"
+    });
+};
+
+document.getElementById("contact").onclick = function () {
+    window.scrollTo({
+        top: 1936,
+        behavior: "smooth"
+    });
+};
