761ea038ce6ea1cfcdcd5e3c9fa4d06661335a8f
Aidan <aidanbh16@gmail.com>
2025-03-26 21:52:35 -0400

    Added center block animation

--- public/scripts/gridAnimations.js
+++ public/scripts/gridAnimations.js
@@ -4,31 +4,34 @@ window.addEventListener("scroll", function(){
         if(this.window.scrollY >= 700){
             setTimeout(function() {
                 document.getElementById("grid1").classList.add("grid1SlideAnimation");
-            }, 1050);
+            }, 1350);
             setTimeout(function() {
                 document.getElementById("grid2").classList.add("grid2SlideAnimation");
-            }, 650);
+            }, 950);
             setTimeout(function() {
                 document.getElementById("grid3").classList.add("grid3SlideAnimation");
-            }, 500);
+            }, 800);
             setTimeout(function() {
                 document.getElementById("grid4").classList.add("grid4SlideAnimation");
-            }, 850);
+            }, 1150);
             setTimeout(function() {
                 document.getElementById("grid5").classList.add("grid5SlideAnimation");
-            }, 350);
+            }, 650);
             setTimeout(function() {
-                document.getElementById("grid7").classList.add("grid7SlideAnimation");
+                document.getElementById("grid6").classList.add("grid6SlideAnimation");
             }, 0);
             setTimeout(function() {
+                document.getElementById("grid7").classList.add("grid7SlideAnimation");
+            }, 300);
+            setTimeout(function() {
                 document.getElementById("grid8").classList.add("grid8SlideAnimation");
-            }, 400);
+            }, 700);
             setTimeout(function() {
                 document.getElementById("grid9").classList.add("grid9SlideAnimation");
-            }, 200);
+            }, 600);
             setTimeout(function() {
                 document.getElementById("grid10").classList.add("grid10SlideAnimation");
-            }, 600);
+            }, 900);
             animationCheck = true;
         }
     }
