0ae8b9c05ce96f998bdf2dc0415220b097d8e452
Aidan <aidanbh16@gmail.com>
2025-03-26 21:59:08 -0400

    animation timing adjustments

--- public/scripts/gridAnimations.js
+++ public/scripts/gridAnimations.js
@@ -4,16 +4,16 @@ window.addEventListener("scroll", function(){
         if(this.window.scrollY >= 700){
             setTimeout(function() {
                 document.getElementById("grid1").classList.add("grid1SlideAnimation");
-            }, 1350);
+            }, 1250);
             setTimeout(function() {
                 document.getElementById("grid2").classList.add("grid2SlideAnimation");
-            }, 950);
+            }, 850);
             setTimeout(function() {
                 document.getElementById("grid3").classList.add("grid3SlideAnimation");
             }, 800);
             setTimeout(function() {
                 document.getElementById("grid4").classList.add("grid4SlideAnimation");
-            }, 1150);
+            }, 1050);
             setTimeout(function() {
                 document.getElementById("grid5").classList.add("grid5SlideAnimation");
             }, 650);
@@ -22,16 +22,16 @@ window.addEventListener("scroll", function(){
             }, 0);
             setTimeout(function() {
                 document.getElementById("grid7").classList.add("grid7SlideAnimation");
-            }, 300);
+            }, 200);
             setTimeout(function() {
                 document.getElementById("grid8").classList.add("grid8SlideAnimation");
-            }, 700);
-            setTimeout(function() {
-                document.getElementById("grid9").classList.add("grid9SlideAnimation");
             }, 600);
             setTimeout(function() {
+                document.getElementById("grid9").classList.add("grid9SlideAnimation");
+            }, 500);
+            setTimeout(function() {
                 document.getElementById("grid10").classList.add("grid10SlideAnimation");
-            }, 900);
+            }, 800);
             animationCheck = true;
         }
     }
