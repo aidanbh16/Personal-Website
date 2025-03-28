666d8a0ae5752b53f116c1c9d58ba1294ddb5ae9
Aidan <aidanbh16@gmail.com>
2025-03-27 20:10:59 -0400

    Added info, techicons, and fixed up animation timing and behavior

--- public/scripts/gridAnimations.js
+++ public/scripts/gridAnimations.js
@@ -4,34 +4,34 @@ window.addEventListener("scroll", function(){
         if(this.window.scrollY >= 700){
             setTimeout(function() {
                 document.getElementById("grid1").classList.add("grid1SlideAnimation");
-            }, 1250);
+            }, 950);
             setTimeout(function() {
                 document.getElementById("grid2").classList.add("grid2SlideAnimation");
-            }, 850);
-            setTimeout(function() {
-                document.getElementById("grid3").classList.add("grid3SlideAnimation");
-            }, 800);
-            setTimeout(function() {
-                document.getElementById("grid4").classList.add("grid4SlideAnimation");
-            }, 1050);
-            setTimeout(function() {
-                document.getElementById("grid5").classList.add("grid5SlideAnimation");
             }, 650);
             setTimeout(function() {
+                document.getElementById("grid3").classList.add("grid3SlideAnimation");
+            }, 550);
+            setTimeout(function() {
+                document.getElementById("grid4").classList.add("grid4SlideAnimation");
+            }, 800);
+            setTimeout(function() {
+                document.getElementById("grid5").classList.add("grid5SlideAnimation");
+            }, 350);
+            setTimeout(function() {
                 document.getElementById("grid6").classList.add("grid6SlideAnimation");
-            }, 0);
+            }, 100);
             setTimeout(function() {
                 document.getElementById("grid7").classList.add("grid7SlideAnimation");
             }, 200);
             setTimeout(function() {
                 document.getElementById("grid8").classList.add("grid8SlideAnimation");
-            }, 600);
+            }, 400);
             setTimeout(function() {
                 document.getElementById("grid9").classList.add("grid9SlideAnimation");
-            }, 500);
+            }, 300);
             setTimeout(function() {
                 document.getElementById("grid10").classList.add("grid10SlideAnimation");
-            }, 800);
+            }, 500);
             animationCheck = true;
         }
     }
