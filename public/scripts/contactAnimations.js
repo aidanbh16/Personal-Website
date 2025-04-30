7b9e23cd4cd491e67618642fd55340eaf1f0e83f
Aidan <aidanbh16@gmail.com>
2025-04-30 11:38:28 -0400

    Pausing this

--- public/scripts/contactAnimations.js
+++ public/scripts/contactAnimations.js
@@ -1,29 +1,31 @@
 let contactAnimationCheck = false;
 window.addEventListener("scroll", function(){
-    if (contactAnimationCheck === false){
-        if(this.window.scrollY >= 2100){
-            setTimeout(function() {
-                document.getElementById("grid13").classList.add("grid13SlideAnimation");
-            }, 1200);
-            setTimeout(function() {
-                document.getElementById("grid16").classList.add("grid16SlideAnimation");
-            }, 1200);
-            setTimeout(function() {
-                document.getElementById("grid11").classList.add("grid11SlideAnimation");
-            }, 800);
-            setTimeout(function() {
-                document.getElementById("grid14").classList.add("grid14SlideAnimation");
-            }, 600);
-            setTimeout(function() {
-                document.getElementById("grid15").classList.add("grid15SlideAnimation");
-            }, 400);
-            setTimeout(function() {
-                document.getElementById("message").classList.add("textAreaSlideAnimation");
-            }, 550);
-            setTimeout(function() {
-                document.getElementById("grid17").classList.add("grid17SlideAnimation");
-            }, 200);
-            contactAnimationCheck = true;
+    if(this.window.innerWidth >= 768){
+        if (contactAnimationCheck === false){
+            if(this.window.scrollY >= 2100){
+                setTimeout(function() {
+                    document.getElementById("grid13").classList.add("grid13SlideAnimation");
+                }, 1200);
+                setTimeout(function() {
+                    document.getElementById("grid16").classList.add("grid16SlideAnimation");
+                }, 1200);
+                setTimeout(function() {
+                    document.getElementById("grid11").classList.add("grid11SlideAnimation");
+                }, 800);
+                setTimeout(function() {
+                    document.getElementById("grid14").classList.add("grid14SlideAnimation");
+                }, 600);
+                setTimeout(function() {
+                    document.getElementById("grid15").classList.add("grid15SlideAnimation");
+                }, 400);
+                setTimeout(function() {
+                    document.getElementById("message").classList.add("textAreaSlideAnimation");
+                }, 550);
+                setTimeout(function() {
+                    document.getElementById("grid17").classList.add("grid17SlideAnimation");
+                }, 200);
+                contactAnimationCheck = true;
+            }
         }
     }
 })
--- public/scripts/gridAnimations.js
+++ public/scripts/gridAnimations.js
@@ -1,38 +1,40 @@
 let gridAnimationCheck = false;
 window.addEventListener("scroll", function(){
     if (gridAnimationCheck === false){
-        if(this.window.scrollY >= 900){
-            setTimeout(function() {
-                document.getElementById("grid1").classList.add("grid1SlideAnimation");
-            }, 950);
-            setTimeout(function() {
-                document.getElementById("grid2").classList.add("grid2SlideAnimation");
-            }, 650);
-            setTimeout(function() {
-                document.getElementById("grid3").classList.add("grid3SlideAnimation");
-            }, 550);
-            setTimeout(function() {
-                document.getElementById("grid4").classList.add("grid4SlideAnimation");
-            }, 800);
-            setTimeout(function() {
-                document.getElementById("grid5").classList.add("grid5SlideAnimation");
-            }, 350);
-            setTimeout(function() {
-                document.getElementById("grid6").classList.add("grid6SlideAnimation");
-            }, 100);
-            setTimeout(function() {
-                document.getElementById("grid7").classList.add("grid7SlideAnimation");
-            }, 200);
-            setTimeout(function() {
-                document.getElementById("grid8").classList.add("grid8SlideAnimation");
-            }, 400);
-            setTimeout(function() {
-                document.getElementById("grid9").classList.add("grid9SlideAnimation");
-            }, 300);
-            setTimeout(function() {
-                document.getElementById("grid10").classList.add("grid10SlideAnimation");
-            }, 500);
-            gridAnimationCheck = true;
+        if(this.window.innerWidth >= 768){
+            if(this.window.scrollY >= 900){
+                setTimeout(function() {
+                    document.getElementById("grid1").classList.add("grid1SlideAnimation");
+                }, 950);
+                setTimeout(function() {
+                    document.getElementById("grid2").classList.add("grid2SlideAnimation");
+                }, 650);
+                setTimeout(function() {
+                    document.getElementById("grid3").classList.add("grid3SlideAnimation");
+                }, 550);
+                setTimeout(function() {
+                    document.getElementById("grid4").classList.add("grid4SlideAnimation");
+                }, 800);
+                setTimeout(function() {
+                    document.getElementById("grid5").classList.add("grid5SlideAnimation");
+                }, 350);
+                setTimeout(function() {
+                    document.getElementById("grid6").classList.add("grid6SlideAnimation");
+                }, 100);
+                setTimeout(function() {
+                    document.getElementById("grid7").classList.add("grid7SlideAnimation");
+                }, 200);
+                setTimeout(function() {
+                    document.getElementById("grid8").classList.add("grid8SlideAnimation");
+                }, 400);
+                setTimeout(function() {
+                    document.getElementById("grid9").classList.add("grid9SlideAnimation");
+                }, 300);
+                setTimeout(function() {
+                    document.getElementById("grid10").classList.add("grid10SlideAnimation");
+                }, 500);
+                gridAnimationCheck = true;
+            }
         }
     }
 })
